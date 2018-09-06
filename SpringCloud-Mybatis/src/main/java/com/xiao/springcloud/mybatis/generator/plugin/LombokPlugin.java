90e0b37c4e32eda33770dda3f0ac3d1bf9e1c719
Administrator <llxiao@winnermedical.com>
2018-09-06 08:34:05 +0800

    mybatis

---
+++ SpringCloud-Mybatis/src/main/java/com/xiao/springcloud/mybatis/generator/plugin/LombokPlugin.java
@@ -0,0 +1,87 @@
+package com.xiao.springcloud.mybatis.generator.plugin;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.Interface;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class LombokPlugin extends PluginAdapter
+{
+
+    @Override
+    public boolean validate(List<String> list)
+    {
+        return true;
+    }
+
+    @Override
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable)
+    {
+        //添加domain的import
+        topLevelClass.addImportedType("lombok.Data");
+        topLevelClass.addImportedType("lombok.Builder");
+        topLevelClass.addImportedType("lombok.NoArgsConstructor");
+        topLevelClass.addImportedType("lombok.AllArgsConstructor");
+
+        //添加domain的注解
+        topLevelClass.addAnnotation("@Data");
+        topLevelClass.addAnnotation("@Builder");
+        topLevelClass.addAnnotation("@NoArgsConstructor");
+        topLevelClass.addAnnotation("@AllArgsConstructor");
+
+        //添加domain的注释
+        topLevelClass.addJavaDocLine("/**");
+        topLevelClass.addJavaDocLine("* Created by Mybatis Generator on " + date2Str(new Date()));
+        topLevelClass.addJavaDocLine("*/");
+        return true;
+    }
+
+    @Override
+    public boolean clientGenerated(Interface interfaze, TopLevelClass topLevelClass,
+            IntrospectedTable introspectedTable)
+    {
+        //Mapper文件的注释
+        interfaze.addJavaDocLine("/**");
+        interfaze.addJavaDocLine("* Created by Mybatis Generator on " + date2Str(new Date()));
+        interfaze.addJavaDocLine("*/");
+        return true;
+    }
+
+    @Override
+    public boolean modelSetterMethodGenerated(Method method, TopLevelClass topLevelClass,
+            IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType)
+    {
+        //不生成getter
+        return false;
+    }
+
+    @Override
+    public boolean modelGetterMethodGenerated(Method method, TopLevelClass topLevelClass,
+            IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType)
+    {
+        //不生成setter
+        return false;
+    }
+
+    @Override
+    public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn,
+            IntrospectedTable introspectedTable, ModelClassType modelClassType)
+    {
+        field.addJavaDocLine("//" + introspectedColumn.getRemarks());
+        return true;
+    }
+
+    private String date2Str(Date date)
+    {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        return sdf.format(date);
+    }
+
+}
