34f00b6fcb845bb7cd25eb15e5e831b46590c94d
Linlin xiao <84226733@qq.com>
2018-10-29 20:27:52 +0800

    Update bootstrap.sh
    优化，参数公共提取

--- SpringCloud-Common/script/bootstrap.sh
+++ SpringCloud-Common/script/bootstrap.sh
@@ -1,83 +1,89 @@
 #!/bin/bash
+## 加载配置，避免获取不到java_home 
+source /etc/profile
 
-## app home path
-SERVICE_HOME=/home/service
-## app name
+SERVICE_HOME=/home/omni-services
 SERVICE_NAME=content-management-service
 
 cd $SERVICE_HOME/$SERVICE_NAME
 PROG=$SERVICE_HOME/$SERVICE_NAME
-PIDFILE=$SERVICE_HOME/$SERVICE_NAME/$SERVICE_NAME.pid
+PIDFILE=$SERVICE_HOME/$SERVICE_NAME/$SERVICE_NAME.pid  
 JARFILE=$SERVICE_HOME/$SERVICE_NAME/$SERVICE_NAME.jar
 
-
-## get app status
-status() {
-    if [ -f $PIDFILE ]; then
-        PID=$(cat $PIDFILE)
-        if [ ! -x /proc/${PID} ]; then
-            return 1
+  
+status() {  
+    if [ -f $PIDFILE ]; then  
+        PID=$(cat $PIDFILE)  
+        if [ ! -x /proc/${PID} ]; then  
+            return 1  
+        else  
+            return 0  
+        fi  
+    else  
+        return 1  
+    fi  
+}  
+  
+case "$1" in  
+    start)  
+        status  
+        RETVAL=$?  
+        if [ $RETVAL -eq 0 ]; then  
+            echo "-----$PIDFILE exists, process is already running or crashed"  
+            exit 1  
+        fi  
+        
+        ##检测 java环境
+        if [ ! -n $JAVA_HOME ]; then
+           echo "-----Please check JAVA_HOME!"
+           echo "-----Exist"
+           exist 1
         else
-            return 0
-        fi
-    else
-        return 1
-    fi
-}
+           echo "-----Jave home: $JAVA_HOME"
+           echo "-----Starting $PROG ..." 
+           nohup java -server -Xms512m -Xmx512m -jar $JARFILE  > $SERVICE_NAME.log 2>&1 &
 
-## check and start,stop,status
-case "$1" in
-    start)
-        status
-        RETVAL=$?
-        if [ $RETVAL -eq 0 ]; then
-            echo "$PIDFILE exists, process is already running or crashed"
-            exit 1
+           RETVAL=$?
+           if [ $RETVAL -eq 0 ]; then
+               echo "-----$PROG is started"
+               echo $! > $PIDFILE
+               exit 0
+           else
+               echo "-----Stopping $PROG"
+               rm -f $PIDFILE
+               exit 1
+           fi        
         fi
-
-        echo "Starting $PROG ..."
-        nohup  $JAVA_HOME/bin/java -server -Xms512m -Xmx512m -jar $JARFILE > $SERVICE_NAME.log 2>&1 &
-
-        RETVAL=$?
-        if [ $RETVAL -eq 0 ]; then
-            echo "$PROG is started"
-            echo $! > $PIDFILE
-            exit 0
-        else
-            echo "Stopping $PROG"
-            rm -f $PIDFILE
-            exit 1
-        fi
-        ;;
-    stop)
-        status
-        RETVAL=$?
-        if [ $RETVAL -eq 0 ]; then
-            echo "Shutting down $PROG"
-            kill `cat $PIDFILE`
-            RETVAL=$?
-            if [ $RETVAL -eq 0 ]; then
-                rm -f $PIDFILE
-            else
-                echo "Failed to stopping $PROG"
-            fi
-        fi
-        ;;
-    status)
-        status
-        RETVAL=$?
-        if [ $RETVAL -eq 0 ]; then
-            PID=$(cat $PIDFILE)
-            echo "$PROG is running ($PID)"
-        else
-            echo "$PROG is not running"
-        fi
-        ;;
-    restart)
-        $0 stop
-        $0 start
-        ;;
-    *)
-        echo "Usage: $0 {start|stop|restart|status}"
-        ;;
-esac
+        ;;  
+    stop)  
+        status  
+        RETVAL=$?  
+        if [ $RETVAL -eq 0 ]; then  
+            echo "-----Shutting down $PROG"  
+            kill -9 `cat $PIDFILE`  
+            RETVAL=$?  
+            if [ $RETVAL -eq 0 ]; then  
+                rm -f $PIDFILE  
+            else  
+                echo "-----Failed to stopping $PROG"  
+            fi  
+        fi  
+        ;;  
+    status)  
+        status  
+        RETVAL=$?  
+        if [ $RETVAL -eq 0 ]; then  
+            PID=$(cat $PIDFILE)  
+            echo "-----$PROG is running ($PID)"  
+        else  
+            echo "-----$PROG is not running"  
+        fi  
+        ;;  
+    restart)  
+        $0 stop  
+        $0 start  
+        ;;  
+    *)  
+        echo "Usage: $0 {start|stop|restart|status}"  
+        ;;  
+esac  
