4ec9bda25d9005c30345cb2295d709957f3de1a7
llxiao <llxiao@winnermedical.com>
2020-01-06 14:36:45 +0800

    脚本调整

--- SpringCloud-Demo-Doc/持续集成/monitor-jstat.sh
+++ SpringCloud-Demo-Doc/持续集成/monitor-jstat.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-##定时任务：-------------
-##每5分钟跑一次脚本，日志输出到monitor.log文件
-#crontab -e
-##输入以下内容：
-#*/5 * * * * sh /home/admin/monitor-jstat.sh > /data/logs/monitor/monitor.log
+## 定时监控本机器下所有java应用的 JVM信息
+# 定时任务配置
+# crontab -e
+# 写入如下，5分钟执行一次检测
+# */5 * * * * /home/admin/monitor-jstat.sh >> /data/logs/monitor/monitor.log 2>&1
+
+#导入环境变量，需要查看当前的用户的环境变量，否则会出现jps ifconfig环境变量不可用错误提示
+export PATH=$PATH:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/data/jdk1.8.0_181/bin:/home/admin/.local/bin:/home/admin/bin
 
 # 老年代告警阈值
-oldwarn="96000"
+oldwarn="99000"
 # 元空间(永久代)告警阈值
 metawarn="99900"
 
 # 手机号码，多个用逗号隔开
-mobile=
+mobile="17727821863"
 # 短信发送url地址
-sendsms=
+sendsms="http://api.pureh2b.com/dreamWebSms/batchSend"
 jsonhead="Content-type:application/json"
 
 ## 当前日期
@@ -22,19 +25,18 @@ sysdate=`date`
 ## 当前服务器IP地址
 addr=`ifconfig -a|grep -w inet|grep -v 127.0.0.1|grep -v 172.16.1.1|awk '{print $2}'|cut -d : -f 2`
 
-## 可以指定忽略某些进程比如Jps和logstash-watch-collection应用，使用grep -v 参数
 for pid in `jps -v |grep -v Jps |grep -v logstash-watch-collection |awk '{print $1}'`
 
 do
- ## 应用名称 忽略Jps和 logstash-watch-collection应用
+ ## 应用名称
  name=`jps -v |grep -v Jps |grep -v logstash-watch-collection |grep $pid |awk '{print $2}'`
- 
+
  #jvm内存监控
- echo "##################开始Java应用:${name}内存监控############################"
- echo "监控日期：     $sysdate"
- echo "进程PID：      $pid"
- echo "进程名称：     $name"
- echo "当前服务器IP： $addr"
+ #echo "##################开始Java应用:${name}内存监控############################"
+ #echo "监控日期：     $sysdate"
+ #echo "进程PID：      $pid"
+ #echo "进程名称：     $name"
+ #echo "当前服务器IP： $addr"
 
  # 堆栈信息
  jstat=$(jstat -gcutil ${pid})
@@ -52,11 +54,11 @@ do
  # 永久代 JDK8已经废弃改用 元空间代替
  meta=$(echo ${jstat}|awk '{print $16}')
 
- echo "Eden Space:           ${eden}%"
- echo "From Space:           ${from}%"
- echo "To Space:             ${tospc}%"
- echo "Old Generation:       ${oldge}%"
- echo "Meta Space:           ${meta}%"
+ #echo "Eden Space:           ${eden}%"
+ #echo "From Space:           ${from}%"
+ #echo "To Space:             ${tospc}%"
+ #echo "Old Generation:       ${oldge}%"
+ #echo "Meta Space:           ${meta}%"
 
  ## 将读取到的各区的值*1000，以便做下一次的比较操作
  #eden1=`gawk -v x=$eden -v y=1000 'BEGIN{printf "%.0f\n",x*y}'`
@@ -86,7 +88,7 @@ do
  #then
    #jvm=$jvm+${name}+":To Space:"+${topspc}+"%超阀值 "
  #fi
-  
+
  # 仅监控 老年代和元空间，超过90以上报警
  jvm=
  if [ ${oldge1} -ge ${oldwarn} ]
@@ -103,12 +105,19 @@ do
    echo "应用:${name}-JVM信息正常"
  else
    jvm="应用${name}的JVM告警信息：${jvm}"
+
+   echo "监控日期：     $sysdate"
+   echo "进程PID：      $pid"
+   echo "进程名称：     $name"
+   echo "当前服务器IP： $addr"
+   echo $jvm
+
    #echo "JVM告警信息：${jvm}"
-   smsContent='{"mobile":"'$mobile'","content":"'$jvm'","userid":"11111","pwd":"123456"}'
+   smsContent='{"mobile":"'$mobile'","content":"'$jvm'","userid":"WL0001","pwd":"123456"}'
    #echo "JVM告警发送短信内容: ${smsContent}"
    # -s 不输出请求信息
    smsResult=$(curl -H ${jsonhead} -X POST -s -d "${smsContent}" ${sendsms})
-   echo "短信返回结果：${smsResult}"
- fi   
- echo "##################结束Java应用:${name}内存监控############################"
+   #echo "短信返回结果：${smsResult}"
+ fi
+ #echo "##################结束Java应用:${name}内存监控############################"
 done
